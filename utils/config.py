155b378a083a43eba7336aa1b80f93bf0a61b3af
seginusaplha <ayush123srivastav890@gmail.com>
2025-09-12 01:36:00 +0530

    Removed the your-folder-name folder

--- utils/config.py
+++
@@ -1,8 +0,0 @@
-import google.generativeai as genai
-
-# ---------------- CONFIG ----------------
-API_KEY = "YOUR_GEMINI_KEY"
-genai.configure(api_key=API_KEY)
-
-# Model name
-LLM_MODEL = "gemini-1.5-flash"
